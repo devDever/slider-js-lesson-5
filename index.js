e8fab40b0137e06810dadabd0d98b6204ebd0d9d
isa <caesar787898@gmail.com>
2022-12-16 15:11:05 +0600

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,37 @@ const slide = sliderList.querySelector('div');
 const leftButton = document.querySelector('.left-btn');
 const rightButton = document.querySelector('.right-btn');
 
+let incrementSlidePostion = 0;
+
 function toLeft() {
 
+    if (incrementSlidePostion >= 415) {
+        return;
+    }
+
+    if (incrementSlidePostion >= 0) {
+        incrementSlidePostion += slide.clientWidth + 5;
+        sliderList.style.transform = 'translateX(-' + incrementSlidePostion + 'px)';
+    }
 }
+
+function toRight() {
+
+    if (incrementSlidePostion === 0) {
+        return;
+    }
+
+
+    if (incrementSlidePostion > 0) {
+        incrementSlidePostion -= slide.clientWidth + 5;
+        sliderList.style.transform = 'translateX(-' + incrementSlidePostion + 'px)';
+        return;
+    }
+
+    incrementSlidePostion += slide.clientWidth + 5;
+    sliderList.style.transform = 'translateX(' + incrementSlidePostion + 'px)';
+}
+
+
+leftButton.addEventListener('click', () => toLeft());
+rightButton.addEventListener('click', () => toRight());
