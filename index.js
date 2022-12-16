7eb83626d432ecd84421ecdfdde2b5fe90c80856
isa <caesar787898@gmail.com>
2022-12-16 14:19:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const sliderList = document.querySelector('.slider-list');
+const slide = sliderList.querySelector('div');
+const leftButton = document.querySelector('.left-btn');
+const rightButton = document.querySelector('.right-btn');
+
+function toLeft() {
+
+}
